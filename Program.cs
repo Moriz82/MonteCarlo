d013c9e4e3fb208c036c141c7fdf3363a9db3ce7
willi <william.dedominico72@k12.leanderisd.org>
2021-08-27 10:27:52 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int stake, goal, itterations, trials = 0, win = 0;
+            int[] bets;
+
+            stake = Int32.Parse(args[0]);
+            goal = Int32.Parse(args[1]);
+            itterations = Int32.Parse(args[2]);
+            
+            bets = new int[itterations];
+
+            for (int i = 0; i < itterations; i++)
+            {
+                int betz = 0;
+                int money = stake;
+                while (true)
+                {
+                    int coinValue = random.Next(2); 
+
+                    betz++;
+                    
+                    if (coinValue == 1)
+                    {
+                        money++;
+                    }
+                    else
+                    {
+                        money--;
+                    }
+
+                    trials++;
+
+                    if (money >= goal)
+                    {
+                        win++;
+                        bets[i] = betz;
+                        break;
+                    }
+
+                    if (money <= 0)
+                    {
+                        bets[i] = betz;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("With "+itterations+" Trials, The average chance of winning is "+(double)win/itterations+" ");
+        }
+    }
+}
